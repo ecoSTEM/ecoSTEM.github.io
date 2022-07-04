2ce13e136d082b200a0c05544a794c2c091cd761
ecoSTEM <ygn.projectc@gmail.com>
2022-07-04 17:48:19 +0800

    audio

--- toggle.js
+++ toggle.js
@@ -6,15 +6,18 @@ function toggle() {
   const box1 = document.getElementById("box1");
   const box2 = document.getElementById("box2");
   const box3 = document.getElementById("box3");
+  const box4 = document.getElementById("box4");
+
   box1.style.visibility = "visible";
+  box2.style.visibility = "hidden";
+  box3.style.visibility = "hidden";
+  box4.style.visibility = "hidden";
   setTimeout(() => {
-    box1.style.visibility = "hidden";
     box2.style.visibility = "visible";
     setTimeout(() => {
-      box2.style.visibility = "hidden";
       box3.style.visibility = "visible";
       setTimeout(() => {
-        box3.style.visibility = "hidden";
+        box4.style.visibility = "visible";
       }, 5000);
     }, 5000);
   }, 5000);
